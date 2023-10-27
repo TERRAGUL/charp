aeea74dc95fba1f1a99c842e4887416eb4dcad81
TERRAGUL <144831235+TERRAGUL@users.noreply.github.com>
2023-10-27 20:06:18 +0300

    Add files via upload

---
+++ togtiki/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+
+namespace togtiki
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Метод в классе программ 
+        {
+            Zakaz.neMain();
+        }
+        
+    }
+}
